06ee3adf9b702a54c57ca58d29b49a68e6c62e41
yukihirop <te108186@gmail.com>
2020-06-14 17:10:07 +0900

    Modify packages/bundler

--- packages/bundler/src/core/types.ts
+++ packages/bundler/src/core/types.ts
@@ -4,11 +4,11 @@ export type Asset = {
   dependencies: string[];
   code: any;
   mapping?: { [key: string]: number };
-}
+};
 
 export type Graph = Array<Asset>;
 export type Modules = Array<string>;
 export type BundlerParams = {
   input: string;
   output: string;
-}
+};

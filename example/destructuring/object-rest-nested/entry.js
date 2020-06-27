ab91f322cade89cd2d0ad8438ddfc73fc6befb57
yukihirop <te108186@gmail.com>
2020-06-27 14:28:18 +0900

    Modify object-rest-nested fixtures

--- example/destructuring/object-rest-nested/entry.js
+++ example/destructuring/object-rest-nested/entry.js
@@ -1,1 +1,3 @@
 var { a, b, c, ...d } = { a: 1, b: { b1: '2' }, c: { c1: true, c2: null, c3: undefined }, d: { d1: { d2: function () { } } }, e: Error, f: WebAssembly }
+const { e, f, g, ...h } = { a: 1, b: { b1: '2' }, c: { c1: true, c2: null, c3: undefined }, d: { d1: { d2: function () { } } }, e: Error, f: WebAssembly }
+let { i, j, k, ...l } = { a: 1, b: { b1: '2' }, c: { c1: true, c2: null, c3: undefined }, d: { d1: { d2: function () { } } }, e: Error, f: WebAssembly }
